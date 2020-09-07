879066fe9f61a9c6420e7ffe6edd548f178ec0ac
arthurlink <artie.link@gmail.com>
2020-09-07 17:06:00 -0600

    Doodling

--- Weather/Weather data.r
+++ Weather/Weather data.r
@@ -1,3 +1,15 @@
 #Author: A.Link
 #Date: 9.7.2020
-#
+#Packages: rvest
+#Notes: NOAA seems to be the only site that provides adequate amounts of data
+
+#How to extract certain data - Dev tools does not seem like it gives much on NOAA
+
+#Try to extract states:
+install.packages("rvest")
+library(rvest)
+list=read_html("https://www.ncdc.noaa.gov/cdo-web/datatools/normals")
+
+#Precipitation transfers (temps-times ratio and measurments: https://www.weather.gov/gsp/snow)
+
+html_nodes(list, 'US States#title)
